0747a3819a77b36c5a9ea387f01578501003f399
etocore <cote.rhys@gmail.com>
2025-07-23 22:14:01 +0000

    fix: corrected JSX icon usage in error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,7 @@
-'use client'; // Error components must be Client Components
+'use client';
 
 import * as React from 'react';
-import { RiAlarmWarningFill as Icon } from 'react-icons/ri';
+import { RiAlarmWarningFill } from 'react-icons/ri';
 
 import TextButton from '@/components/buttons/TextButton';
 
@@ -12,16 +12,14 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
-  React.useEffect(() => {
-    // eslint-disable-next-line no-console
-    console.error(error);
-  }, [error]);
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  React.useEffect(() => {}, [error]);
 
   return (
     <main>
       <section className='bg-white'>
         <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
-          <Icon
+          <RiAlarmWarningFill
             size={60}
             className='drop-shadow-glow animate-flicker text-red-500'
           />
